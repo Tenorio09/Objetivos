3d9b6b5215b8eca39773b391b5e9fb55394751c3
Tenorio09 <eneas.matias@escola.pr.gov.br>
2026-05-13 12:13:13 -0300

    .

--- main.js
+++ main.js
@@ -14,3 +14,32 @@ for(let i = 0; i<botoes.length;i++){
 
     }
 }
+
+const contadores = document.querySelectorAll(".contador")
+const tempoObjetivo1 = new Date("2026-10-05T00:00:00")
+const tempoObjetivo2 = new Date("2026-11-05T00:00:00")
+const tempoObjetivo3 = new Date("2026-12-05T00:00:00")
+const tempoObjetivo4 = new Date("2027-01-05T00:00:00")
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual
+    console.log(tempoFinal)
+    let segundos = Math.floor(tempoFinal / 1000)
+    let minutos = Math.floor(segundos / 60)
+    let hotas = Math.floor(minutos / 60)
+    let dias = Math.floor (horas / 24)
+
+    segundos %= 60
+    minutos %= 60
+    horas %=24
+    if (tempoFinal > 0){
+        return [dias, horas, minutos, segundos]    
+    }else {
+        return [0,0,0,0]
+    }
+}
+
+calculaTempo(tempoObjetivo1)
